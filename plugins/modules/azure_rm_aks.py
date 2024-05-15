7835e129bbdb6a9035d8bcdb5f7c6a413afcc9c3
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2024-05-15 17:39:32 +0800

    Add managedNATGateway/userAssignedNATGateway to outbound_type property of the azure_rm_aks module (#1537)

--- plugins/modules/azure_rm_aks.py
+++ plugins/modules/azure_rm_aks.py
@@ -268,6 +268,8 @@ options:
                 choices:
                     - loadBalancer
                     - userDefinedRouting
+                    - managedNATGateway
+                    - userAssignedNATGateway
     api_server_access_profile:
         description:
             - Profile of API Access configuration.
@@ -835,7 +837,7 @@ network_profile_spec = dict(
     dns_service_ip=dict(type='str'),
     docker_bridge_cidr=dict(type='str'),
     load_balancer_sku=dict(type='str', choices=['standard', 'basic']),
-    outbound_type=dict(type='str', default='loadBalancer', choices=['userDefinedRouting', 'loadBalancer'])
+    outbound_type=dict(type='str', default='loadBalancer', choices=['userDefinedRouting', 'loadBalancer', 'userAssignedNATGateway', 'managedNATGateway'])
 )
 
 
