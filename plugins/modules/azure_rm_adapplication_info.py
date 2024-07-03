2c9aeab15b475b37162ba7b338d4e79aab303d71
TheNotoriousRMM <164023381+TheNotoriousRMM@users.noreply.github.com>
2024-07-03 08:00:34 +0200

    Feature azure rm adapplication info diff (#1560)
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    app_diff hinzugefügt
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    Anpassungen
    
    * Anpassungen
    
    * Anpassungen
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    Finalizing
    
    modified:   tests/integration/targets/azure_rm_adapplication/tasks/main.yml
    Add tests for app_diff
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    modify doc for sentry test
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    documentation modify examples
    
    * modified:   tests/integration/targets/azure_rm_adapplication/tasks/main.yml
    Fix: No new line character at the end of file
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    Add new documentation for the option app_diff
    
    * modified:   plugins/modules/azure_rm_adapplication_info.py
    Fix wrong indentation
    
    * Update tests/integration/targets/azure_rm_adapplication/tasks/main.yml
    
    I hope this will resolve the issue.
    
    Co-authored-by: Fred-sun <37327967+Fred-sun@users.noreply.github.com>
    
    * modified:   tests/integration/targets/azure_rm_adapplication/tasks/main.yml
    Change No new line character at the end of file
    
    * modified:   tests/integration/targets/azure_rm_adapplication/tasks/main.yml
    Undo changes at the last line. Sanity test was OK.
    
    * modified:   tests/integration/targets/azure_rm_adapplication/tasks/main.yml
    Add new line at the end of the file. Opened in vim and save, the new line automaticly added.
    
    ---------
    
    Co-authored-by: MehrR <raphael.mehr@stadtluzern.ch>
    Co-authored-by: baechir <raphael.baechi@stadtluzern.ch>
    Co-authored-by: TheRapac <55585899+therapac@users.noreply.github.com>
    Co-authored-by: Fred-sun <37327967+Fred-sun@users.noreply.github.com>

--- plugins/modules/azure_rm_adapplication_info.py
+++ plugins/modules/azure_rm_adapplication_info.py
@@ -34,8 +34,20 @@ options:
         type: str
     app_display_name:
         description:
-            - The applications' Name.
+            - The application Name.
         type: str
+    app_diff:
+        description:
+            - A list of applications
+            - The application name or application ID is mandatory
+            - All fields of the applications can also be provided (parsing from a JSON or YAML).
+            - With this option, you can compare your self-defined applications with the current state using an application list.\
+              The applications that are present only in current state but not in your list will be returned as a list with all fields\
+              and will receive an additional status of ABSENT.\
+              This allows you to first add applications with azure_rm_adapplication and then use azure_rm_adapplication_info (option diff)\
+              to identify the applications that should not be in the current state and subsequently delete them with azure_rm_adapplication
+        type: list
+        elements: dict
 
 extends_documentation_fragment:
     - azure.azcollection.azure
@@ -63,6 +75,12 @@ EXAMPLES = '''
 - name: get ad app info ---- by display name
   azure_rm_adapplication_info:
     app_display_name: "{{ display_name }}"
+
+- name: get ad app diff ---- by display name
+  azure_rm_adapplication_info:
+    app_diff:
+      - app_display_name: "{{ display_name }}"
+      - app_id: "{{ app_id }}"
 '''
 
 RETURN = '''
@@ -153,6 +171,99 @@ applications:
                     type: list
                     returned: always
                     sample: ['name': 'acct', 'source': null, 'essential': false, 'additional_properties': []]
+app_diff:
+    description:
+        - The info of the ad application.
+    type: complex
+    returned: aways
+    contains:
+        app_display_name:
+            description:
+                - Object's display name or its prefix.
+            type: str
+            returned: always
+            sample: app
+        app_id:
+            description:
+                - The application ID.
+            returned: always
+            type: str
+            sample: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        identifier_uris:
+            description:
+                - The identifiers_uri list of app.
+            type: list
+            returned: always
+            sample: ["http://ansible-atodorov"]
+        object_id:
+            description:
+                - It's application's object ID.
+            returned: always
+            type: str
+            sample: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+        sign_in_audience:
+            description:
+                - The application can be used from any Azure AD tenants
+            type: str
+            returned: always
+            sample: AzureADandPersonalMicrosoftAccount
+        available_to_other_tenants:
+            description:
+                - The application can be used from any Azure AD tenants
+            type: str
+            returned: always
+            sample: AzureADandPersonalMicrosoftAccount
+        public_client_reply_urls:
+            description:
+                - The public client redirect urls.
+                - Space-separated URIs to which Azure AD will redirect in response to an OAuth 2.0 request.
+            returned: always
+            type: list
+            sample: []
+        web_reply_urls:
+            description:
+                - The web redirect urls.
+                - Space-separated URIs to which Azure AD will redirect in response to an OAuth 2.0 request.
+            returned: always
+            type: list
+            sample: []
+        spa_reply_urls:
+            description:
+                - The spa redirect urls.
+                - Space-separated URIs to which Azure AD will redirect in response to an OAuth 2.0 request.
+            returned: always
+            type: list
+            sample: []
+        state:
+            description:
+                - absent --> The app isn't in the app_diff.
+            returned: always
+            type: str
+            sample: absent
+        optional_claims:
+            description:
+                - Declare the optional claims for the application.
+            type: complex
+            returned: always
+            contains:
+                access_token_claims :
+                    description:
+                        - The optional claims returned in the JWT access token
+                    type: list
+                    returned: always
+                    sample: ['name': 'aud', 'source': null, 'essential': false, 'additional_properties': []]
+                id_token_claims:
+                    description:
+                        - The optional claims returned in the JWT ID token
+                    type: list
+                    returned: always
+                    sample: ['name': 'acct', 'source': null, 'essential': false, 'additional_properties': []]
+                saml2_token_claims:
+                    description:
+                        - The optional claims returned in the SAML token
+                    type: list
+                    returned: always
+                    sample: ['name': 'acct', 'source': null, 'essential': false, 'additional_properties': []]
 '''
 
 from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common_ext import AzureRMModuleBase
@@ -173,7 +284,8 @@ class AzureRMADApplicationInfo(AzureRMModuleBase):
             app_id=dict(type='str'),
             object_id=dict(type='str'),
             identifier_uri=dict(type='str'),
-            app_display_name=dict(type='str')
+            app_display_name=dict(type='str'),
+            app_diff=dict(type='list', elements='dict')
         )
         self.app_id = None
         self.app_display_name = None
@@ -191,7 +303,6 @@ class AzureRMADApplicationInfo(AzureRMModuleBase):
             setattr(self, key, kwargs[key])
 
         applications = []
-
         try:
             self._client = self.get_msgraph_client()
             if self.object_id:
@@ -207,12 +318,27 @@ class AzureRMADApplicationInfo(AzureRMModuleBase):
                 apps = asyncio.get_event_loop().run_until_complete(self.get_applications(sub_filters))
                 applications = list(apps)
             self.results['applications'] = [self.to_dict(app) for app in applications]
+            current_app = [self.to_dict(app) for app in applications]
         except APIError as e:
             if e.response_status_code != 404:
                 self.fail("failed to get application info {0}".format(str(e)))
-        except Exception as ge:
-            self.fail("failed to get application info {0}".format(str(ge)))
-
+        except Exception as e:
+            if "Name or service not known" in str(e):
+                self.fail("DNS resolution error occurred.")
+            else:
+                self.fail("An unexpected error occurred: {0}".format(str(e)))
+        if self.app_diff:
+            temp_app = []
+            for app in current_app:
+                found = False
+                for diff in self.app_diff:
+                    if app.get("app_id") == diff.get("app_id") or app.get("app_display_name") == diff.get("app_display_name"):
+                        found = True
+                        break
+                if not found:
+                    app['state'] = 'absent'
+                    temp_app.append(app)
+            self.results['app_diff'] = temp_app
         return self.results
 
     def serialize_claims(self, claims):
