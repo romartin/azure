6441529f3dec17a207ac395198992882322e234c
Paul Czarkowski <username.taken@gmail.com>
2024-07-22 23:42:40 -0400

    Update azure_rm_openshiftmanagedclusterkubeconfig_info.py (#1631)
    fixes #1630

--- plugins/modules/azure_rm_openshiftmanagedclusterkubeconfig_info.py
+++ plugins/modules/azure_rm_openshiftmanagedclusterkubeconfig_info.py
@@ -119,7 +119,7 @@ class AzureRMOpenShiftManagedClustersKubeconfigInfo(AzureRMModuleBaseExt):
         for key in self.module_arg_spec:
             setattr(self, key, kwargs[key])
 
-        self.mgmt_client = self.get_mgmt_svc_client(GenericRestClient, is_track2=True,
+        self.mgmt_client = self.get_mgmt_svc_client(GenericRestClient,
                                                     base_url=self._cloud_environment.endpoints.resource_manager)
         self.results = self.get_kubeconfig()
         if self.path and self.path_is_valid():
